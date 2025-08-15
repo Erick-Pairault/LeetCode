5e48d3495fc6e0d21f7fed555a86bb400960dda2
Erick Pairault <erick_pairault@hotmail.com>
2025-08-14 20:39:20 -0700

    Added solution that doesn't use loops or recursion.

---
+++ Power of Four/Power Of Four Log.py
@@ -0,0 +1,9 @@
+import math
+
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n > 0 and math.log(n, 4).is_integer()
