8ecae2d58f9bccda27d91cac419a194c2def6d53
Erick Pairault <erick_pairault@hotmail.com>
2025-08-14 19:40:07 -0700

    Solution Attempt 1 Multiple of Fur

--- Power of Four/Power of Four.py
+++ Power of Four/Power of Four.py
@@ -1,1 +1,12 @@
-print("Hello, world!")
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n<= pow(-2,31) or n>= pow(2,31) - 1:
+            return False
+        elif n == 1 or n%4 == 0:
+            return True
+        else:
+            return False
