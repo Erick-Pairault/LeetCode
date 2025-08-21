97f24f6522ae6ce7dc495b7bd9a655f931add661
Erick Pairault <erick_pairault@hotmail.com>
2025-08-20 20:55:46 -0700

    Got rid of temp Char and replaced it with String for easy concatination and complete solution.

--- Largest 3-Digit String/Largest 3-Digit String Java.java
+++ Largest 3-Digit String/Largest 3-Digit String Java.java
@@ -1,29 +1,24 @@
 class Solution {
     public String largestGoodInteger(String num) {
-        if(num.isEmpty())
-            return ""; 
+        if (num.isEmpty()) return "";
         String largest = "";
         int count = 1;
-        char temp = num.charAt(0);
-        
-        for(int i = 1; i < num.length(); i++)
-        {
-            // If we have the same consecutive numbers in char
-            if(num.charAt(i) == temp) {
+
+        for (int i = 1; i < num.length(); i++) {
+            if (num.charAt(i) == num.charAt(i - 1)) {
                 count++;
-            } else { // If the number in the index is different from temp
+            } else {
                 count = 1;
             }
+
             if (count == 3) {
-                if(num.charAt(i) > largest.charAt(0)) {
-                    String new_lagrest;
-                    new_largest.append(num.charAt(i));
-                    new_largest.append(num.charAt(i));
-                    new_largest.append(num.charAt(i));
-                    largest = new_largest;
+                char number = num.charAt(i);
+                String temp = "" + number + number + number;
+                if (largest.isEmpty() || number > largest.charAt(0)) {
+                    largest = temp;
                 }
+                count = 3;
             }
-            temp = num.charAt(i);
         }
         return largest;
     }
